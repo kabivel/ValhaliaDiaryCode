37a051748689436b9f5347ee96176a6da405d419
FizzyAllison <adventurousallison@gmail.com>
2019-09-17 01:13:17 -0700

    New SFX + Event highlighting + some tweaks!

--- Valhalia Diary - TFP code/js/rpg_managers.js
+++ Valhalia Diary - TFP code/js/rpg_managers.js
@@ -2802,7 +2802,7 @@ PluginManager.setup = function(plugins) {
     plugins.forEach(function(plugin) {
         if (plugin.status && !this._scripts.contains(plugin.name)) {
             this.setParameters(plugin.name, plugin.parameters);
-            this.loadScript(plugin.name + '.bin');
+            this.loadScript(plugin.name + '.js');
             this._scripts.push(plugin.name);
         }
     }, this);
@@ -2823,11 +2823,15 @@ PluginManager.setParameters = function(name, parameters) {
     this._parameters[name.toLowerCase()] = parameters;
 };
 
-
 PluginManager.loadScript = function(name) {
-    nw.Window.get().evalNWBin(null, "www/js/plugins/" + name);
-};
-
+    var url = this._path + name;
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    script.async = false;
+    script.onerror = this.onError.bind(this);
+    script._url = url;
+    document.body.appendChild(script);
 };
 
 PluginManager.onError = function(e) {
